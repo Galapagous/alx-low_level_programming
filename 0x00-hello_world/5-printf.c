827f7d89e9d119ebe5033d80f5c76e3bb4016b4a
muhammed <galapagous09@gmail.com>
2022-09-08 19:54:24 -0700

    printing with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program start here.
+ * Description - Printing with the printf function.
+ * Return: 0/nothing.
+ */
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+}
+return (0);

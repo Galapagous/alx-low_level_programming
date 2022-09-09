674618ede191ce89aa4c30a84c574c308b50c710
muhammed <galapagous09@gmail.com>
2022-09-08 19:39:07 -0700

    running my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void){
+/**
+ * Description: printing a file using the put(abstraction) function.
+ * main - Program entry point.
+  */
+int main(void)
+	{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
-
+	}

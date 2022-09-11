35f7c1e26c96a1eb7592dce61c1af4bcec5e512f
muhammed <galapagous09@gmail.com>
2022-09-11 15:12:55 -0700

    print possible combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point.
+ * Description: prints numbers.
+ * Return: 0/nothing.
+ */
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			for (i = '0'; i <= '9' ; i++)
+
+					{
+
+								putchar(i);
+
+
+
+										if (i != '9')
+
+													{
+
+																	putchar(',');
+
+																				putchar(' ');
+
+																						}
+
+											}
+
+
+
+				putchar('\n');
+
+					return (0);
+
+}

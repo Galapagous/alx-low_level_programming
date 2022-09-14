906911fa2bd2884e48be946f1c0d7b36aaa396a1
muhammed <galapagous09@gmail.com>
2022-09-14 11:19:12 -0700

    hot fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower: Program entry point.
+ * _islower-> Program entry point.
  * @c: parameter to be check.
  * Description: Program to return lower case character.
  * Return: 1 if character is lower case else return 0.

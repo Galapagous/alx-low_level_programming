58b10ff045e2505655c8b54edbfde731d3f03178
muhammed <galapagous09@gmail.com>
2022-09-14 22:50:53 -0700

    100times table

---
+++ 0x02-functions_nested_loops/100_times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print multiplication up to params.
+ * @n: parameter to print to.
+ * Return: time table data structure.
+ */
+void print_times_table(int n)
+{
+	int x,y,z;
+	if (n >= 0 && n <= n; n <=14)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y =0; y <= n; y++)
+			{
+				z = x*y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}

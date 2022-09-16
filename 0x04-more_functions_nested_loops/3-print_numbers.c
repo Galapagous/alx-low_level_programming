374195732270e18392e4252e43216ec1edd3ce23
muhammed <galapagous09@gmail.com>
2022-09-15 21:27:08 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - function entry point.
+ *Return: 0/nothing is returned.
+ */
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+		putchar(10);
+}

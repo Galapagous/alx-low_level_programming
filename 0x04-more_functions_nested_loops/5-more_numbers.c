e08f63c9e9be5682b11b3be4ed1bd8c06805f613
muhammed <galapagous09@gmail.com>
2022-09-16 09:43:49 -0700

    printing more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers.
+ * Return: 0/nothing.
+ */
+void more_numbers(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			if (num2 > 9)
+			{
+				putchar((num2 / 10) + '0');
+			}
+			putchar((num2 % 10) + '0');
+		}
+		putchar(10);
+	}
+}

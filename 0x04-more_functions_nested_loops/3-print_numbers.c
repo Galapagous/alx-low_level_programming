42af732c081481bd26ac179d42d83aa3e4e695ce
muhammed <galapagous09@gmail.com>
2022-09-16 21:18:43 -0700

    adding numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - function entry point.
@@ -9,7 +8,7 @@ void print_numbers(void)
 {
 	for (int i = 0; i <= 10; i++)
 	{
-		putchar(i + '0');
+		_putchar(i + '0');
 	}
-		putchar('\n');
+		_putchar('\n');
 }

da470ce6c81e9045ae3e1f6b10ba1c93aca8988f
muhammed <galapagous09@gmail.com>
2022-09-19 02:37:09 -0700

    reset to 98 using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - program to reset a value to 98.
+ * Description : function accept a pointer and update value to 98.
+ * @n : pointer parameter.
+ **/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+Program on pointers arrays and strings
+i. 0-reset_to_98.c : A function that takes a pointer as a parameter and update its value to 98.
+

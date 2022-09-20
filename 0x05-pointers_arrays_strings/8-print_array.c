f06281bcb83cef8942cda4dde302d49bef9ee606
muhammed <galapagous09@gmail.com>
2022-09-20 09:57:36 -0700

    printing string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		i != num - 1 ? printf(", ") : printf("");
+		i != n - 1 ? printf(", ") : 0;
 	}
 	printf("\n");
 }

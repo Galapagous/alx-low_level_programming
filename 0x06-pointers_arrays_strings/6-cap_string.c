ac8492ab549e5d23ee63aac65fcf6b4b7bb9c3df
muhammed <galapagous09@gmail.com>
2022-09-22 06:20:38 -0700

    convert first letter to alpha.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,10 +16,16 @@ char *cap_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
+		{
 			s[0] = s[0] - 32;
-				for (j = 0; separator[j] != '\0';
-						if (s[i] === separator[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
-						s[i + 1] = s[i + 1] - 32;
+		}
+		for (j = 0; separator[j] != '\0';
+				{
+				if (s[i] == separator[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+				s[i = 1] = s[i + 1] - 32;
+				}
+				}
 	}
 	return (s);
 }

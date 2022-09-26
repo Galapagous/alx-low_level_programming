741e97c6cede5e3e311d6a42a86433a5c656515b
muhammed <galapagous09@gmail.com>
2022-09-26 02:43:53 -0700

    copying text from dest to src

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy string.
+ * Description : function to copy a certain num of character form src to dest.
+ * @n : number of character to copy.
+ * @i - looping through string.
+ * @dest : destination to be copied to.
+ * @src : source ofstring to copy.
+ * Return: pointer to destination.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char temp;
+
+	for (i = 0; i < n; i++)
+	{
+		temp = src[i];
+		dest = &temp;
+	}
+	return (dest);
+}

835e4f8d3246fb6a9b263c15ca8eb8db43a2f156
muhammed <galapagous09@gmail.com>
2022-09-28 06:41:03 -0700

    printing with puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function to print to the terminal using put.
+ * Description - printing string of text using puts.
+ * @s : pointer to string of text to print.
+ */
+
+void _puts_recursion(char *s)
+{
+	_puts(s); }
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+
+int _puts(char *s);
+int _putchar(char *s);
+void _puts_recursion(char *s);
+
+#endif

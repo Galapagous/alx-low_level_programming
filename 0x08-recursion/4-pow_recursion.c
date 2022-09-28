63cb6346494815665fda1382b9930e0a8c2e4c55
muhammed <galapagous09@gmail.com>
2022-09-28 11:22:35 -0700

    raise to power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function to mimick raise to power.
+ * @x: The base integer.
+ * @y:The exponential integer.
+ * Return: The result of X^Y as an int.
+ */
+
+int pow_recursion(int x, int y)
+{
+	if (x == 1)
+		return (1);
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x* _pow_recursion(x, (y - 1)));
+}

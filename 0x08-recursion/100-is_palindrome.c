ad215596ac2c5bcff6af6a8fd870741cc5d31d21
muhammed <galapagous09@gmail.com>
2022-09-29 08:20:27 -0700

    palindrome using recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to find length of string.
+ * @s: pointer to string parameter.
+ * Return: an integer of string length.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * pali - palidrome
+ * @s: pointer to string
+ * @i: position
+ * Return: boolean
+ */
+
+int pali(char *s, int i)
+{
+	if (i < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + i))
+	{
+		return (pali(s + 1, i - 2));
+	}
+	return (0);
+}
+
+/**
+ * is _palindrome - palindrome checker
+ * @s: pointer to string
+ * Return: recursion.
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (pali(s, len - 1));
+}
